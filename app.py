345e314fc48797751c5abeb1540896742133e9d2
devasol2008 <solankidevanshi80@gmail.com>
2026-03-23 19:03:50 +0530

    day 2 : login + dashboard +level1 cmplete

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
-from flask import Flask, render_template , request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Home Page
 @app.route("/")
 def home():
     return render_template("index.html")
-@app.route("/login",methods=["GET","POST"])
+
+# Login Page
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    
-     if request.method == "POST":
-        print("From submitted")
-        username = request.form["username"]
-        password = request.form["password"]
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
         if username == "admin" and password == "1234":
-            return "Login Successful 🎉"
+            return redirect(url_for("dashboard"))
         else:
             return "Invalid Credentials ❌"
 
-     return render_template("login.html")
+    return render_template("login.html")
 
+# Dashboard Page
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+# Level 1 Page
+@app.route("/level1")
+def level1():
+    return render_template("level1.html")
+
+# Run App
 if __name__ == "__main__":
     app.run(debug=True)
-    
